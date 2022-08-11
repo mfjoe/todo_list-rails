9557fd9478035e2fba0e206b4cecc5d248fa8b2e
Josef Hare-Brown <josefhb@hotmail.com>
2022-08-11 08:37:25 +0100

    moving laptop

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,19 @@
+class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create
+  end
+
+end
